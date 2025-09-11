c34bf8b1204281978db5fb1665d40005d1757fda
chsoares <cordial.road4051@fastmail.com>
2025-09-11 00:54:31 -0300

    gum colors

--- functions/_ezpz_colors.fish
+++ functions/_ezpz_colors.fish
@@ -13,22 +13,22 @@
 #     echo (set_color red --bold)"  "$argv(set_color normal)
 # end
 # function ezpz_warn
-#     echo (set_color magenta --bold)"  "$argv(set_color normal)
+#     echo (set_color magenta --bold)"  "$argv(set_color normal)
 # end
 # function ezpz_success
-#     echo (set_color magenta --bold)"  "$argv(set_color normal)
+#     echo (set_color magenta --bold)" "$argv(set_color normal)
 # end
 # function ezpz_question
 #     echo (set_color cyan)"  "$argv(set_color normal)
 # end
 # function ezpz_title
-#     echo (set_color magenta --bold)"  "$argv(set_color normal)
+#     echo (set_color magenta --bold)"  "$argv(set_color normal)
 # end
 
 # gum spin --spinner minidot --title " running..." --spinner.foreground 6 --title.foreground 4 --show-stdout --
 # gum style --foreground 5 --border-foreground 6 --border rounded --padding "0 4" ' ezpz netscan v0.fish'
 
-# no special symbols
+no special symbols
 
 function ezpz_header
     echo (set_color yellow --bold)"[+] "$argv(set_color normal)
